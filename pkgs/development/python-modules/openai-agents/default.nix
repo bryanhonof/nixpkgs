5db16e6c860219ce0a9a71cdc324cea1c0675a0a
Bryan Honof <bryanhonof@gmail.com>
2025-07-27 02:30:19 +0200

    python3Packages.openai-agents: 0.0.13 -> 0.2.3
    Also moved over to the finalAttrs approach of doing this, since I liked it
    better than having a recursive attribute set. Also got rid of nix-update-script,
    since it didn't seem to be doing anything?

--- pkgs/development/python-modules/openai-agents/default.nix
+++ pkgs/development/python-modules/openai-agents/default.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  nix-update-script,
   buildPythonPackage,
   fetchPypi,
   hatchling,
@@ -13,47 +12,43 @@
   typing-extensions,
 }:
 
-buildPythonPackage rec {
-  pname = "openai-agents";
-  version = "0.0.13";
-  pyproject = true;
+let
+  finalAttrs = {
+    pname = "openai-agents";
+    version = "0.2.3";
+    pyproject = true;
 
-  src = fetchPypi {
-    inherit version;
-    pname = "openai_agents";
-    hash = "sha256-a4AxXnXAa1MCxfKtui+eo4RflGFdrtRwa/uHF0D1YaU=";
+    src = fetchPypi {
+      inherit (finalAttrs) version;
+      pname = "openai_agents";
+      hash = "sha256-ldStGUxcDPGkADjLcB7ujs2q92mNh7sT48LFz/gMS00=";
+    };
+
+    build-system = [
+      hatchling
+    ];
+
+    dependencies = [
+      griffe
+      mcp
+      openai
+      pydantic
+      requests
+      types-requests
+      typing-extensions
+    ];
+
+    pythonImportsCheck = [
+      "agents"
+    ];
+
+    meta = {
+      changelog = "https://github.com/openai/openai-agents-python/releases/tag/${finalAttrs.version}";
+      homepage = "https://github.com/openai/openai-agents-python";
+      description = "A lightweight, powerful framework for multi-agent workflows";
+      license = lib.licenses.mit;
+      maintainers = [ lib.maintainers.bryanhonof ];
+    };
   };
-
-  # OpenAI 1.76.0 seems to not build currently
-  postPatch = ''
-    substituteInPlace pyproject.toml --replace-fail "openai>=1.76.0" "openai"
-  '';
-
-  build-system = [
-    hatchling
-  ];
-
-  dependencies = [
-    griffe
-    mcp
-    openai
-    pydantic
-    requests
-    types-requests
-    typing-extensions
-  ];
-
-  pythonImportsCheck = [
-    "agents"
-  ];
-
-  passthru.updateScript = nix-update-script { };
-
-  meta = {
-    changelog = "https://github.com/openai/openai-agents-python/releases/tag/${version}";
-    homepage = "https://github.com/openai/openai-agents-python";
-    description = "A lightweight, powerful framework for multi-agent workflows";
-    license = lib.licenses.mit;
-    maintainers = [ lib.maintainers.bryanhonof ];
-  };
-}
+in
+buildPythonPackage finalAttrs
